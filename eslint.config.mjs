1093d341e694c52a3df8d29ea3a5d7e1471496f7
Matheus Elias <mathelias1@gmail.com>
2025-10-06 10:24:05 -0300

    Init project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,52 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+/** @type {import('eslint').Linter.Config[]} */
+const config = [
   {
     ignores: [
-      "node_modules/**",
+      "coverage/**",
       ".next/**",
-      "out/**",
-      "build/**",
+      "public/**",
+      "next.config.js",
       "next-env.d.ts",
     ],
   },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node, React: true },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  ...compat.config({
+    extends: ["next"],
+    settings: {
+      next: {
+        rootDir: ".",
+      },
+    },
+  }),
+  {
+    files: ["**/*.{jsx,tsx,ts,js}"],
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "no-console": "warn",
+    },
+  },
 ];
-
-export default eslintConfig;
+export default config;
